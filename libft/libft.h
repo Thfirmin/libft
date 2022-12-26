8e30b14030cf1fc1848d271c5ecda703df14883c
Thfirmin <thfirmin.dsantos@gmail.com>
2022-12-26 13:49:59 -0300

    error fix: include bonus mod into libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: thfirmin <thfirmin@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/22 16:21:04 by thfirmin          #+#    #+#             */
-/*   Updated: 2022/12/20 19:15:23 by thfirmin         ###   ########.fr       */
+/*   Updated: 2022/12/26 13:15:19 by thfirmin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,18 +22,20 @@ typedef struct s_list
 	struct s_list	*next;
 }					t_list;
 
+// Data analysis
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
 int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
 
+// Data convertion
 int		ft_toupper(int c);
 int		ft_tolower(int c);
 int		ft_atoi(const char *str);
 char	*ft_itoa(int n);
 
-int		ft_nbrlen(int n);
+// String manipulation
 size_t	ft_strlen(const char *str);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strchr(const char *s, int c);
@@ -49,6 +51,7 @@ char	*ft_strdup(const char *s1);
 void	ft_striteri(char *s, void (*f) (unsigned int, char *));
 char	*ft_strmapi(char const *s, char (*f) (unsigned int, char));
 
+// Memory manipullation
 void	*ft_memset(void *b, int c, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
@@ -57,13 +60,18 @@ void	*ft_memmove(void *dst, const void *src, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t count, size_t size);
 
+// Output data
 int		ft_putchar_fd(char c, int fd);
 int		ft_putstr_fd(char *s, int fd);
 int		ft_putendl_fd(char *s, int fd);
 int		ft_putnbr_fd(int n, int fd);
 
+// Personal functions
+int		ft_nbrlen(int n);
 char	*ft_isall(char *str, int (*is)(int));
+void	ft_frsplit(char **split);
 
+// Linked list manipulaiton
 t_list	*ft_lstnew(void *content);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
